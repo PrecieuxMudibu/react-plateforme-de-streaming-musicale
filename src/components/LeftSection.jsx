327173a915688c2a7c47bb21dad7bb88d8dd4772
PrecieuxMudibu <oceangpmb@gmail.com>
2022-09-23 23:33:53 +0200

    Use NavLink for my leftsection

--- src/components/LeftSection.jsx
+++ src/components/LeftSection.jsx
@@ -1,6 +1,6 @@
 import logo from "../images/logo.png"
 import "../styles/leftSection.css"
-import { Link } from "react-router-dom"
+import {NavLink} from "react-router-dom"
 import { FiHome } from "react-icons/fi"
 import { RiPlayListFill } from "react-icons/ri"
 import { BsHeartFill, BsSearch } from "react-icons/bs"
@@ -10,7 +10,6 @@ import { applicationContext } from "../App"
 function LeftSection() {
     const { activeLink, updateActiveLink } = useContext(applicationContext)
 
-    if (activeLink == "accueil") {
         return (
             <div className="left-section">
                 <div className="page__logo-group">
@@ -22,67 +21,30 @@ function LeftSection() {
                     <span className="page__logo-text">Musiker</span>
                 </div>
                 <nav className="page__navigation">
-                    <Link
-                        className="page__link page__link--active"
+                    <NavLink
+                        className="page__link"
                         to="/accueil"
                     >
                         <span className="icon">
                             <FiHome />
                         </span>
                         <span className="page__link-text">Accueil</span>
-                    </Link>
-                    <Link className="page__link" to="/playlists">
+                    </NavLink>
+                    <NavLink className="page__link" to="/playlists">
                         <span className="icon">
                             <RiPlayListFill />
                         </span>
                         <span className="page__link-text">Playlists</span>
-                    </Link>
-                    {/* <Link className="page__link" to="/chansons-aimees">
+                    </NavLink>
+                    <NavLink className="page__link" to="/chansons-aimees">
                         <span className="icon">
                             <BsHeartFill />
                         </span>
                         <span className="page__link-text">Chansons aimées</span>
-                    </Link> */}
+                    </NavLink>
                 </nav>
             </div>
         )
-    } else if (activeLink == "playlists") {
-        return (
-            <div className="left-section">
-                <div className="page__logo-group">
-                    <img
-                        className="page__logo-img"
-                        src={logo}
-                        alt="logo de Musiker"
-                    />
-                    <span className="page__logo-text">Musiker</span>
-                </div>
-                <nav className="page__navigation">
-                    <Link className="page__link" to="/accueil">
-                        <span className="icon">
-                            <FiHome />
-                        </span>
-                        <span className="page__link-text">Accueil</span>
-                    </Link>
-                    <Link
-                        className="page__link page__link--active"
-                        to="/playlists"
-                    >
-                        <span className="icon">
-                            <RiPlayListFill />
-                        </span>
-                        <span className="page__link-text">Playlists</span>
-                    </Link>
-                    {/* <Link className="page__link" to="/chansons-aimées">
-                        <span className="icon">
-                            <BsHeartFill />
-                        </span>
-                        <span className="page__link-text">Chansons aimées</span>
-                    </Link> */}
-                </nav>
-            </div>
-        )
-    }
 }
 
 export default LeftSection
